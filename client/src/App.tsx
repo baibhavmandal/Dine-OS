f4f43f9d1707fd004042269579ec4972c562381b
baibhavmandal <baibhavmandal1@gmail.com>
2025-05-07 05:04:59 +0000

    Client side all set and ready for development

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>Client side all set and ready for development</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
